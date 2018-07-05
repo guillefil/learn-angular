45a0525f4410dfcfc285641ece4583e659ca9537
Guillermo Filia <gfilia@pragmaconsultores.net>
2018-07-04 21:12:21 -0300

    Header Component

---
+++ 01-hola-mundo/src/app/components/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-header', //define qué etiqueta de HTML lo carga
+    template: `<h1>Header Component</h1>`
+})
+export class HeaderComponent{
+    // esto mapea HTML a componentes angular
+
+}
